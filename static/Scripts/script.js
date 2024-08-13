3c660d32eb8abbf67da4ecbbd5fe04c8c4877196
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-08-13 20:48:15 +0300

    Home frontend

--- static/Scripts/script.js
+++ static/Scripts/script.js
@@ -1,14 +1,45 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function(event) 
+{
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
 
-    if (username === 'admin' && password === 'password') {
+    if (email === 'admin@gmail.com' && password === 'password')
+    {
         alert('Login successful!');
         errorMessage.textContent = '';
-    } else {
+    }
+     else 
+    {
         errorMessage.textContent = 'Invalid username or password';
     }
 });
+
+function searchTable() 
+{
+    let input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("searchBar");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("inventoryTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) 
+    {
+        tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) 
+        {
+            if (td[j]) 
+            {
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) 
+                {
+                    tr[i].style.display = "";
+                    break;
+                }
+            }
+        }
+    }
+}
