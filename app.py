b61cc23421a1d297da19f2aa0bdca7a60ec958ad
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-08-16 04:41:57 +0300

    Some frontend adjustments & login is now functioning

--- app.py
+++ app.py
@@ -1,12 +1,69 @@
-from flask import Flask
-import psycopg2
+from flask import Flask, render_template, request, redirect, session
+import psycopg2.extras
+from classes import User, Admin, Item, ElectricalPart, MechanicalPart, RawMaterial
 
 app = Flask(__name__)
+app.secret_key = "Youssef.8.3"
 
+database_session = psycopg2.connect(
+     database="postgres",
+     port=5432,
+     host="localhost",
+     user="postgres",
+     password="Youssef.8.3"
+)
+database_session.autocommit = True
+cursor = database_session.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def retrieve_user(email, password):
+    cursor.execute("SELECT * FROM general_user WHERE email=%s AND password=%s", (email, password))
+    database_user = cursor.fetchone()
+    print(f"Database User: {database_user}")
+    if database_user:
+        return database_user
+    return None
+
+def retrieve_items(id):
+    cursor.execute("SELECT admin_role FROM general_user WHERE id=%s", (id,))
+    admin_role = cursor.fetchone()
+    if admin_role.get('admin_role'):
+        cursor.execute("SELECT * FROM item")
+    else:
+        cursor.execute("SELECT * FROM item WHERE user_id=%s", (id,))
+    database_items = cursor.fetchall()
+    if database_items:
+        return database_items
+    return None
+
+@app.route('/', methods=['GET'])
+def default():
+    return render_template("login.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print(f"Email: {email}, Password: {password}")
+    user = retrieve_user(email, password)
+    print(f"User: {user}")
+    if user:
+        items = retrieve_items(user.get('id'))
+        print(f"Items: {items}")
+        session['user'] = user
+        if user.get('admin_role'):
+            return render_template('admin.html', user = user, items = items)
+        return render_template('/home.html', user = user, items = items)
+    session['user'] = None
+    error_message = "Invalid email or password. Please try again."
+    return render_template("login.html", error=error_message)
+
+@app.route('/home', methods=['POST'])
+def home():
+    return render_template("home.html", user=session.get('user'))
+
+@app.route('/admin', methods=['POST'])
+def admin():
+    return render_template("admin.html", user=session.get('user'))
 
 
 if __name__ == '__main__':
---
+++ classes.py
@@ -0,0 +1,79 @@
+from abc import ABC, abstractmethod
+
+class User:
+    def __init__(self, id, first_name, last_name, email, phone, address, birthdate, gender, ssn, admin_role=False, password="1234", my_items=[]):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.birthdate = birthdate
+        self.gender = gender
+        self.ssn = ssn
+        self.admin_role = admin_role
+        self.password = password
+        self.my_items = my_items
+
+    def update_details(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                if key == "admin_role":
+                    continue
+                setattr(self, key, value)
+        return self
+    
+class Admin(User):
+    def __init__(self, id, first_name, last_name, email, phone, address, birthdate, gender, ssn, admin_role=True, password="1234", items_list=[]):
+        super().__init__(id, first_name, last_name, email, phone, address, birthdate, gender, ssn, admin_role, password, items_list)
+        self.items_list = items_list
+
+
+
+class Item(ABC):
+    def __init__(self, id, name, quantity, price, user_id):
+        self.id = id
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+        self.user_id = user_id
+
+    @abstractmethod
+    def update_details(self, **kwargs):
+        pass
+
+class ElectricalPart(Item):
+    def __init__(self, id, name, description, quantity, price, user_id):
+        super().__init__(id, name, description, quantity, price, user_id)
+        self.description = description
+        self.type = "Electrical"
+
+    def update_details(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        return self        
+
+class MechanicalPart(Item):
+    def __init__(self, id, name, description, quantity, price, user_id):
+        super().__init__(id, name, description, quantity, price, user_id)
+        self.description = description
+        self.type = "Mechanical"
+
+    def update_details(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        return self        
+
+class RawMaterial(Item):
+    def __init__(self, id, name, description, quantity, price, user_id):
+        super().__init__(id, name, description, quantity, price, user_id)
+        self.description = description
+        self.type = "Raw Material"
+
+    def update_details(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        return self        
